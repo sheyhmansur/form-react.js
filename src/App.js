9bbc8a9076115daee6b5ae8cae157cbc2f79238b
Sheyh-Mansur Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2021-12-04 11:14:16 +0300

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [email, setEmail] = useState("");
@@ -7,11 +7,43 @@ const App = () => {
   const [passwordDirty, setPasswordDirty] = useState(false);
   const [emailError, setEmailError] = useState("Email не может быть пустым");
   const [passwordError, setPasswordError] = useState("Пароль не может быть пустым");
-
+  const [formValid, setFormValid] = useState(false);
   // emailDirty - другое состояние, которая означает были мы внутри инпута или нет. По умолчанию будет false, но как только коснемся инпута - должны будем делать его уже true
 
-  // Этот event срабатывает в тот момент, когда пользователь покинул поле ввода, т.е убрал курсор из инпута
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, passwordError]);
 
+  // Когда пользователь пытается что-то ввести надо изменять
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (!re.test(String(e.target.value).toLocaleLowerCase())) {
+      setEmailError("Некорректный email");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 3 || e.target.value.length > 8) {
+      setPasswordError("Пароль должен быть длиннее 3 и меньше 8");
+      if (!e.target.value) {
+        setPasswordError("Пароль не может быть пустым!");
+      }
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  // Этот event срабатывает в тот момент, когда пользователь покинул поле ввода, т.е убрал курсор из инпута
   const blurHandler = (e) => {
     switch (e.target.name) {
       case "email":
@@ -29,6 +61,8 @@ const App = () => {
         <h1>Регистрация</h1>
         {emailDirty && emailError && <div style={{ color: "red" }}>{emailError}</div>}
         <input
+          onChange={(e) => emailHandler(e)}
+          value={email}
           onBlur={(e) => blurHandler(e)}
           name="email"
           type="text"
@@ -36,12 +70,16 @@ const App = () => {
         />
         {passwordDirty && passwordError && <div style={{ color: "red" }}>{passwordError}</div>}
         <input
+          onChange={(e) => passwordHandler(e)}
+          value={password}
           onBlur={(e) => blurHandler(e)}
           name="password"
           type="password"
           placeholder="Введите ваш пароль..."
         />
-        <button>registartion</button>
+        <button disabled={!formValid} type="submit">
+          registartion
+        </button>
       </form>
     </div>
   );
