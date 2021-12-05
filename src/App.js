5871103b5683021b22138fca46aaf03a71fa27b5
Sheyh-Mansur Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2021-12-05 11:12:55 +0300

    added regex for input name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [nameDirty, setNameDirty] = useState(false);
+  const [nameError, setNameError] = useState("Поле ввода не должно быть пустым!");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [emailDirty, setEmailDirty] = useState(false);
-  const [passwordDirty, setPasswordDirty] = useState(false);
   const [emailError, setEmailError] = useState("Email не может быть пустым");
-  const [passwordError, setPasswordError] = useState("Пароль не может быть пустым");
   const [formValid, setFormValid] = useState(false);
+  // const [password, setPassword] = useState("");
+  // const [passwordDirty, setPasswordDirty] = useState(false);
+  // const [passwordError, setPasswordError] = useState("Пароль не может быть пустым");
   // emailDirty - другое состояние, которая означает были мы внутри инпута или нет. По умолчанию будет false, но как только коснемся инпута - должны будем делать его уже true
 
   useEffect(() => {
-    if (emailError || passwordError) {
+    if (emailError || nameError) {
       setFormValid(false);
     } else {
       setFormValid(true);
     }
-  }, [emailError, passwordError]);
+  }, [emailError, nameError]);
 
-  // Когда пользователь пытается что-то ввести надо изменять
+  const nameHandler = (e) => {
+    setName(e.target.value);
+    e.target.value = e.target.value.replace(/[0-9\+\=\\/``\*]|\./, "");
+    if (!e.target.value) {
+      setNameError("Поле обязательно для заполнения");
+    } else {
+      setNameError("");
+    }
+  };
+
   const emailHandler = (e) => {
     setEmail(e.target.value);
     const re =
@@ -31,26 +43,26 @@ const App = () => {
     }
   };
 
-  const passwordHandler = (e) => {
-    setPassword(e.target.value);
-    if (e.target.value.length < 3 || e.target.value.length > 8) {
-      setPasswordError("Пароль должен быть длиннее 3 и меньше 8");
-      if (!e.target.value) {
-        setPasswordError("Пароль не может быть пустым!");
-      }
-    } else {
-      setPasswordError("");
-    }
-  };
+  // const passwordHandler = (e) => {
+  //   setPassword(e.target.value);
+  //   if (e.target.value.length < 3 || e.target.value.length > 8) {
+  //     setPasswordError("Пароль должен быть длиннее 3 и меньше 8");
+  //     if (!e.target.value) {
+  //       setPasswordError("Пароль не может быть пустым!");
+  //     }
+  //   } else {
+  //     setPasswordError("");
+  //   }
+  // };
 
   // Этот event срабатывает в тот момент, когда пользователь покинул поле ввода, т.е убрал курсор из инпута
   const blurHandler = (e) => {
     switch (e.target.name) {
+      case "name":
+        setNameDirty(true);
+        break;
       case "email":
         setEmailDirty(true);
-        break;
-      case "password":
-        setPasswordDirty(true);
         break;
     }
   };
@@ -59,6 +71,16 @@ const App = () => {
     <div className="app">
       <form>
         <h1>Регистрация</h1>
+        {nameDirty && nameError && <div style={{ color: "red" }}>{nameError}</div>}
+        <input
+          onChange={(e) => nameHandler(e)}
+          onInput={(e) => nameHandler(e)}
+          value={name}
+          onBlur={(e) => blurHandler(e)}
+          name="name"
+          type="text"
+          placeholder="Введите ваше имя..."
+        />
         {emailDirty && emailError && <div style={{ color: "red" }}>{emailError}</div>}
         <input
           onChange={(e) => emailHandler(e)}
@@ -68,15 +90,15 @@ const App = () => {
           type="text"
           placeholder="Введите ваш email..."
         />
-        {passwordDirty && passwordError && <div style={{ color: "red" }}>{passwordError}</div>}
+        {/* {passwordDirty && passwordError && <div style={{ color: "red" }}>{passwordError}</div>}
         <input
           onChange={(e) => passwordHandler(e)}
           value={password}
           onBlur={(e) => blurHandler(e)}
           name="password"
           type="password"
-          placeholder="Введите ваш пароль..."
-        />
+          placeholder="Введите ваш номер телефона"
+        /> */}
         <button disabled={!formValid} type="submit">
           registartion
         </button>
